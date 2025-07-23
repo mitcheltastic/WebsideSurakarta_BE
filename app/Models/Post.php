4dc96b2439edd5d355c2c1059dd1bfc43bc57142
Mitchel <mitchel.mohamad@gmail.com>
2025-07-23 23:22:31 +0530

    duar

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'author_id',
+        'type',
+        'status',
+        'featured_image_path',
+    ];
 }
